7b7844279394f65066305b471e498e3960400bf8
Conor <conor@conorfox.com>
2021-02-04 18:56:19 +0000

    Lab 1

---
+++ lab1/Note.java
@@ -0,0 +1,12 @@
+public class Note {
+    private final String note;
+
+    public Note( final String text ) {
+        note = text;
+    }
+
+    @Override
+    public String toString( ) {
+        return note;
+    }
+}
