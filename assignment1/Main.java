af67c2fc60fa715bde979cad6ecfa43e003fc26c
Conor Fox <conor@conorfox.com>
2021-03-08 18:54:50 +0000

    Create objects in main

--- assignment1/Main.java
+++ assignment1/Main.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         final List<Garment> items = new ArrayList<Garment>();
-        items.add( new Jeans( ) );
+        items.add( new Jeans() );
+        items.add( new TShirt() );
+        items.add( new Jacket() );
+        items.add( new RainCoat() );
+        items.add( new WinterCoat() );
 
         for (Garment item : items) {
             item.printPurpose();
