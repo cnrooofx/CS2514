c9a4f027a2eb2bf127c77c1726c85d5e15e38528
Conor Fox <conor@conorfox.com>
2021-03-12 17:44:47 +0000

    Move comment to the class

--- assignment1/Main.java
+++ assignment1/Main.java
@@ -1,12 +1,11 @@
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Test for all the methods for the classes.
  * 
  * @author Conor Fox (ID 119322236)
  */
-
-import java.util.List;
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         final List<Garment> items = new ArrayList<Garment>();
