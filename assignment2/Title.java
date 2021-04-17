3437997fc575ec74c81d00a5c45fbc3290bcf80d
Conor Fox <conor@conorfox.com>
2021-04-17 22:02:49 +0100

    Title

---
+++ assignment2/Title.java
@@ -0,0 +1,22 @@
+public class Title {
+    private final String title;
+    private final String subtitle;
+    
+    public Title(final String title, final String subtitle) {
+        this.title = title;
+        this.subtitle = subtitle;
+    }
+
+    public Title(final String title) {
+        this.title = title;
+        this.subtitle = null;
+    }
+
+    public String getTitle() {
+        String titleString = this.title;
+        if (this.subtitle != null) {
+            titleString += " / " + this.subtitle;
+        }
+        return titleString;
+    }
+}
